311ef7b939fcb82c2f687cb782770b0376806233
CristianmMst <cristianmmst@gmail.com>
2023-05-11 19:51:05 -0500

    remove gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://CristianmMst.github.io",
-  base: "/Portfolio",
 });
