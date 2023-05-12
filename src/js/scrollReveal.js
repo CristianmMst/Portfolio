abb76bb761f3969acaa2d291b1fcf1fd8e0b13b0
CristianmMst <cristianmmst@gmail.com>
2023-05-12 17:51:00 -0500

    scrollreveal reset false

--- src/js/scrollReveal.js
+++ src/js/scrollReveal.js
@@ -3,28 +3,24 @@ import ScrollReveal from "scrollreveal";
 const sr = ScrollReveal();
 
 const revealLeft = {
-  reset: true,
   duration: 2000,
   origin: "left",
   distance: "200px",
 };
 
 const revealRight = {
-  reset: true,
   duration: 2000,
   origin: "right",
   distance: "200px",
 };
 
 const revealUp = {
-  reset: true,
   duration: 2000,
   origin: "top",
   distance: "100px",
 };
 
 sr.reveal(".Title", {
-  reset: true,
   duration: 2000,
   origin: "bottom",
   distance: "50px",
